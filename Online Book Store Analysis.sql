14b38e8f690154ea946b60e7b6dd5344ecabf26d
Rinkal2195 <121945139+Rinkal2195@users.noreply.github.com>
2025-08-30 16:44:54 +0530

    Update Online Book Store Analysis.sql

--- Online Book Store Analysis.sql
+++ Online Book Store Analysis.sql
@@ -1,3 +1,67 @@
+
+ Online Book Store Data Analysis - SQL 
+
+### 📌 Overview  
+This project analyzes an **Online Bookstore dataset** using SQL to explore sales performance, customer trends, and inventory management.  
+The goal is to answer key business questions and provide insights into revenue, customers, and book categories.  
+
+---
+
+## 🎯 Objectives  
+- Perform **data cleaning & transformation** using SQL  
+- Write SQL queries to answer **business questions**  
+- Study **customer purchase behavior**  
+- Evaluate **top authors, categories, and revenue drivers** 
+
+---
+
+## 🛠️ Tools & Technologies   
+- **SQL Database**: MySQL 
+- **Tools Used**: MySQL Workbench / pgAdmin / 
+
+---
+
+## 📂 Dataset  
+ 1. `books` – Book details (Book_ID, Title, Author, Genre, Published, Price, Stock)  
+ 2. `customers` – Customer info (Customer_ID, Name, Email, Phone Number, City, Country)  
+ 3. `orders` – Order details (Order_ID, Customer_ID, Book ID, Order_Date, Quantity, Total_Amount)
+
+---
+
+## 🔍 Analysis Workflow  
+1. **Data Cleaning & Preprocessing**  
+   - Removed duplicates & missing values  
+   - Converted order date into proper format  
+   - Extracted `Year`, `Month`, `Day` for time-series analysis  
+
+2. **SQL Analysis Performed**
+    1.**Basic Queries**
+   1) Retrieve all books in the "Fiction" genre
+   2) Find books published after the year 1950
+   3) List all customers from the Canada
+   4) Show orders placed in November 2023
+   5) Retrieve the total stock of books available
+   6) Find the details of the most expensive book
+   7) Show all customers who ordered more than 1 quantity of a book
+   8) Retrieve all orders where the total amount exceeds $20
+   9) List all genres available in the Books table
+  10) Find the book with the lowest stock
+  11) Calculate the total revenue generated from all orders 
+
+  2.**Advance Queries**
+  1) Retrieve the total number of books sold for each genre
+  2) Find the average price of books in the "Fantasy" genre
+  3) List customers who have placed at least 2 orders
+  4) Find the most frequently ordered book
+  5) Show the top 3 most expensive books of 'Fantasy' Genre
+  6) Retrieve the total quantity of books sold by each author
+  7) List the cities where customers who spent over $30 are located
+  8) Find the customer who spent the most on orders
+  9) Calculate the stock remaining after fulfilling all orders
+
+---
+##Code of Project:-
+
 -- Create Database
 --CREATE DATABASE OnlineBookstore;
 
